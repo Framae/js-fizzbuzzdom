651dd7e8456bf8690708067acc9d32ad81e44275
Francesco Maestrelli <framae26@gmail.com>
2022-01-21 20:23:44 +0100

    innerHTML

--- main.js
+++ main.js
@@ -6,4 +6,7 @@ for (let i=1; i<=100; i++) {
 
     containerElement.innerHTML += "<div class='box'>" + i + "</div>"
 
+    // if (i%3==0) {
+    //     containerElement.innerHTML = "<div class='box-red'>"i"</div>"
+    // }
 }
