835ca53c96df5ebf9b6dfc8a562b5e0b76896215
Francesco Maestrelli <framae26@gmail.com>
2022-01-21 20:41:28 +0100

    commentario colori

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ for (let i=1; i<=100; i++) {
 
     containerElement.innerHTML += "<div class='box'>" + i + "</div>"
 
-    // if (i%3==0) {
-    //     containerElement.innerHTML = "<div class='box-red'>"i"</div>"
-    // }
+    if (i%3==0) {
+          console.log(i+" è multiplo di 3")
+    }
 }
+
+//multipli di 3: colore rosso
+
+//multipli di 5: colore verde
+
+//multipli di 3 e 5: colore giallo
