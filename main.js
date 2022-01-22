c5fec21fa50ff120a123a94ddc543ad096c79dbb
Francesco Maestrelli <framae26@gmail.com>
2022-01-22 18:52:37 +0100

    partial results

--- main.js
+++ main.js
@@ -2,23 +2,23 @@ const containerElement = document.getElementById("container")
 
 for (let i=1; i<=100; i++) {
     
-    console.log(`ciclo: ${i}`);
-
     if (i%3==0) {
-        containerElement.innerHTML += "<div class='box-red'>" + i + "</div>"
-    
+        containerElement.innerHTML += "<div class='box-red'>" + "fizz" + i + "</div>"
+        
     }
 
     else if (i%5==0){
-        containerElement.innerHTML += "<div class='box-green'>" + i + "</div>"
+        containerElement.innerHTML += "<div class='box-green'>" + "buzz" + i + "</div>"
+        
     }
 
-    // else if (i%3==0 && i%5==0) {
-    //     containerElement.innerHTML += "<div class='box-yellow'>" + i + "</div>"
-    // }
+    else if (i%5==0 && i%3==0) {
+         containerElement.innerHTML += "<div class='box-yellow'>" + "FizzBuzz" + i + "</div>"
+    }
     else {
         containerElement.innerHTML += "<div class='box'>" + i + "</div>"
     }
+    
 }
 
 //multipli di 3: colore rosso
